96b29d707bca8fe966684a6387fd2e63e4086e66
Damian Chan <d.chan@2009.ljmu.ac.uk>
2013-01-22 14:06:22 +0000

    New Rails 3.2.11 app Soourn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sojourn::Application.config.secret_token = '042d3c47fc7450a3fb2e762067674fbf4d1fe4dac488518c4f21e5cef2b939771259d7bab94b0559969b3afc547ab5d44c8bbd6bc2703bc434ec6ddad6b0595e'
